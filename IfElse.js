d44cb6428cf7afe798939f9b831949a88accee55
DidiDemiray <64772171+DidiDemiray@users.noreply.github.com>
2020-05-05 18:58:44 +0200

    Add files via upload

---
+++ IfElse.js
@@ -0,0 +1,36 @@
+const age= Number; 
+
+Number= 18;
+
+if (age>18) {
+    console.log('Kom binnen')
+} else if (age<18) {
+    console.log('Je komt niet binnen')
+} else {
+    console.log('Kom binnen')
+}
+
+const ifFemale= Boolean;
+
+Boolean= true; 
+
+if (ifFemale== true ) {
+    console.log('KOM BINNEN')
+} else {
+    console.log('BLIJF BUITEN')
+}
+
+const driverStatus= Number; 
+
+Number= 0.6
+
+if (driverStatus==0) {
+    console.log('Je mag door rijden')
+} else {
+    console.log('Je krijgt een bekeuring')
+    
+}
+
+
+
+
